3511fa8d1031e6f5148d1f70c4a52f427955346d
Nitehawk <nite97m@gmail.com>
2018-05-21 19:53:47 -0500

    Rough OLED support - output temp only
    Rough testing version, not yet verified working

--- fs/init.js
+++ fs/init.js
@@ -2,15 +2,32 @@ load('api_config.js');
 load('api_rpc.js');
 load('api_dht.js');
 load('api_timer.js');
+load('api_sensor_utils.js');
+load('api_arduino_ssd1306.js');
 
 let pin = Cfg.get('app.pin');
 let dht = DHT.create(pin, Cfg.get('app.sensor'));
 
+let oled = Adafruit_SSD1306.create_i2c(4, Adafruit_SSD1306.RES_128_32);
+oled.begin(Adafruit_SSD1306.SWITCHCAPVCC, 0x3C, true);
+oled.display();
+
+let OLPrint = function(d, str) {
+	d.clearDisplay();
+	d.setTextSize(2);
+	d.setTextColorBg(Adafruit_SSD1306.WHITE, Adafruit_SSD1306.BLACK);
+	d.setCursor(4, 4);
+	d.write(str);
+	d.display();
+};
+
 Timer.set(1000, true, function() {
 	  let tc = dht.getTemp();
-	  let tf = tc * 9 / 5 + 32;
-	  let hum = dht.getHumidity();
+	  let tf = SensorUtils.fahrenheit(tc);
+	  let temp = Math.round(tc);
+	  let hum = Math.round(dht.getHumidity());
 	  print('Temperature:', tc, 'c    ', tf, 'f     -    Rel Humidity: ', hum, '%');
+	  OLPrint(oled, "Temp: " + JSON.stringify(temp));
 	}, null);
 
 RPC.addHandler('Temp.Read', function(args) {
@@ -20,3 +37,4 @@ RPC.addHandler('Temp.Read', function(args) {
 RPC.addHandler('Humid.Read', function(args) {
 	  return { value: dht.getHumidity() };
 	});
+
