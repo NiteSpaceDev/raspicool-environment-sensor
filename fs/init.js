18a841114db3a2cfa10a9808bfc43ff9e102d6fa
Nitehawk <nite97m@gmail.com>
2018-05-26 17:12:36 -0500

    Working BME280 sensor code
    BME 280 sensor working except for humidity, Code cleanup and refactor due next.

--- fs/init.js
+++ fs/init.js
@@ -1,16 +1,17 @@
 load('api_config.js');
 load('api_rpc.js');
 load('api_gpio.js');
-load('api_dht.js');
 load('api_timer.js');
 load('api_sensor_utils.js');
 load('api_ssd1306.js');
+load('api_bme280.js');
 
+// Turn on BMP / BME 280 sensor
+let sensor = BME280.createSPI();
 
-
-let pin = Cfg.get('app.pin');
-let dht = DHT.create(pin, Cfg.get('app.sensor'));
-
+// On the ESP32 dev board we are using, GPIO pin 16 is connected to the OLED
+//  RST pin.   We pull this pin high to power the OLED
+// TODO - Make this a config element
 GPIO.set_mode(16, GPIO.MODE_OUTPUT);
 GPIO.write(16, GPIO.PULL_UP);
 
@@ -25,19 +26,23 @@ let OLPrint = function(d, str) {
 };
 
 Timer.set(1000, true, function() {
-	  let tc = dht.getTemp();
+	  let tc = sensor.readTemp();
 	  let tf = SensorUtils.fahrenheit(tc);
 	  let temp = Math.round(tc);
-	  let hum = Math.round(dht.getHumidity());
-	  print('Temperature:', tc, 'c    ', tf, 'f     -    Rel Humidity: ', hum, '%');
+	  let hum = Math.round(sensor.readHumid());
+	  let press = sensor.readPress();
+	  print('Temperature:', tc, 'c    ', tf, 'f     -    Rel Humidity: ', hum, '%   Press: ', press);
 	  OLPrint(oled, "Temp: " + JSON.stringify(temp));
 	}, null);
 
 RPC.addHandler('Temp.Read', function(args) {
-	  return { value: dht.getTemp() };
+	  return { value: sensor.readTemp() };
 	});
 
 RPC.addHandler('Humid.Read', function(args) {
-	  return { value: dht.getHumidity() };
+	  return { value: sensor.readHumid() };
 	});
 
+RPC.addHandler('Press.Read', function(args) {
+	  return { value: sensor.readPress() };
+	});
